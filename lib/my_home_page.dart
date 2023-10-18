8012b508f47a81e7ac6b45754516a1e9305f9223
phamtanphat <phatdroid94@gmail.com>
2023-10-18 20:08:57 +0700

    ui

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -14,7 +14,26 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("My home page"),
       ),
-      body: SafeArea(child: Container()),
+      body: SafeArea(
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Count: 0", style: TextStyle(color: Colors.red, fontSize: 30)),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(onPressed: () { }, child: Text("Increase")),
+                    ElevatedButton(onPressed: () { }, child: Text("Decrease")),
+                    ElevatedButton(onPressed: () { }, child: Text("Reset")),
+                  ],
+                )
+              ],
+            ),
+          )
+      ),
     );
   }
 }
