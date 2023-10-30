e61783142c575e5a4efec56c97e8840d65945aff
phamtanphat <phatdroid94@gmail.com>
2023-10-30 20:28:01 +0700

    tach widget

--- lib/to_do_list_page.dart
+++ lib/to_do_list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import 'date_time_utils.dart';
@@ -11,12 +13,12 @@ class TodoListPage extends StatefulWidget {
 
 class _TodoListPageState extends State<TodoListPage> {
 
-  List<Map<String, String>> listTodo = [
-    {"name": "Do homework 1", "time" : formatTime(DateTime.now())},
-    {"name": "Do homework 2", "time" : formatTime(DateTime.now())},
-    {"name": "Do homework 3", "time" : formatTime(DateTime.now())},
-    {"name": "Do homework 4", "time" : formatTime(DateTime.now())},
-    {"name": "Do homework 5", "time" : formatTime(DateTime.now())}
+  List<Map<String, dynamic>> listTodo = [
+    {"name": "Do homework 1", "time" : formatTime(DateTime.now()), "isChecked": false},
+    {"name": "Do homework 2", "time" : formatTime(DateTime.now()), "isChecked": false},
+    {"name": "Do homework 3", "time" : formatTime(DateTime.now()), "isChecked": false},
+    {"name": "Do homework 4", "time" : formatTime(DateTime.now()), "isChecked": false},
+    {"name": "Do homework 5", "time" : formatTime(DateTime.now()), "isChecked": false}
   ];
 
   @override
@@ -30,21 +32,51 @@ class _TodoListPageState extends State<TodoListPage> {
         child: ListView.builder(
             itemCount: listTodo.length,
             itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                  leading: Checkbox(onChanged: (_) { }, value: false),
-                  title: Text(listTodo[index]["name"] ?? "", style: TextStyle(fontSize: 20, color: Colors.teal)),
-                  subtitle: Text(listTodo[index]["time"] ?? "", style: TextStyle(fontSize: 14, color: Colors.red)),
-                  trailing: IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {  },
-                  ),
-
-                )
-              );
+                String name = listTodo[index]["name"] ?? "";
+                String time = listTodo[index]["time"] ?? "";
+                bool isChecked = listTodo[index]["isChecked"] ?? false;
+                return itemTodoWidget(
+                  name: name,
+                  time: time,
+                  isChecked: isChecked,
+                  index: index,
+                  onClickCheckBox: onListenCheckBox,
+                  onClickEdit: onListenEditItem
+                );
             },
         )
       ),
+    );
+  }
+
+  void onListenCheckBox(bool? isChecked, int index) {
+
+  }
+
+  void onListenEditItem(int index) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Click edit item index $index")));
+  }
+
+  Widget itemTodoWidget({
+    required String name,
+    required String time,
+    required bool isChecked,
+    required int index,
+    required Function(bool?, int index) onClickCheckBox,
+    required Function(int index) onClickEdit
+  }) {
+    return Card(
+        child: ListTile(
+          leading: Checkbox(
+              onChanged: (isChecked) => onClickCheckBox(isChecked, index),
+              value: isChecked),
+          title: Text(name, style: const TextStyle(fontSize: 20, color: Colors.teal)),
+          subtitle: Text(time, style: const TextStyle(fontSize: 14, color: Colors.red)),
+          trailing: IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () => onClickEdit(index),
+          ),
+        )
     );
   }
 }
